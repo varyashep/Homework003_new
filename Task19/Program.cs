735f678032e1fc2b4eca5e86bb4b7627dd8cbcd8
Barbara <varya.sheptunova@gmail.com>
2022-12-29 19:17:32 +0300

    Task 19 Finding a palindrome

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+int number;
+WriteLine("Введите пятизначное число:");
+if (int.TryParse(ReadLine(), out number) && number > 999 && number < 100000)
+{
+    if ((number % 10 == (number / 10000)) && ((number / 10) % 10 == (number / 1000) % 10))
+    {
+        WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        WriteLine("Число не является палиндромом");
+    }
+}
+else
+{
+    WriteLine("Ошибка. Необходимо ввести пятизначное число.");
+}
