3c580bdec242525c8d93b0df8af1fce9a40347dc
Barbara <varya.sheptunova@gmail.com>
2022-12-29 19:43:24 +0300

    Task 23 A table of cubes

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console; 
+
+WriteLine("Введите число: ");
+
+int degree = 3;
+int base_number;
+int n = 1;
+
+if (int.TryParse(ReadLine(), out base_number))
+{
+    WriteLine($"Кубы чисел от 1 до {base_number}:");
+    while (n <= base_number)
+    {   
+        WriteLine(Math.Pow(n, degree));
+        n++;
+    }
+}
+else
+{
+    WriteLine("Ошибка. Необходимо ввести число.");
+}
